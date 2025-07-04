d7a6df440427aafe6e80b4eaace2538471769fe4
Roznrasho <roznrasho@gmail.com>
2025-07-04 23:58:58 +0200

    import components and hooks in App.jsx

--- analog-clock-app/src/App.jsx
+++ analog-clock-app/src/App.jsx
@@ -1,4 +1,8 @@
 import './App.css'
+import { useEffect, useRef, useState } from "react";
+import ClockHand from "./components/ClockHand";
+import ClockFace from "./components/ClockFace";
+import AlarmControls from "./components/AlarmControls";
 
 function App() {
  
